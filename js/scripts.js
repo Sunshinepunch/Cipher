972d7b0eb65f6f8881ad25073c0e61e0d010c04f
Becket Harvey <becketfc@gmail.com>
2021-08-09 17:46:07 -0700

    completed up to sixth bulletin

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,26 @@
-function sentence(){
-  const word = prompt("Hello. Enter a sentence.");
-  return word;
+let word = prompt("Enter a sentence");
+console.log(word);
+
+let first = word.charAt(0).toUpperCase();
+let last = word.charAt(word.length-1).toUpperCase();
+
+function capitalize(){
+  return first + last;
   }
   
-  function 
-  
-  console.log(sentence())
+  function reverse(){
+  return last.concat(first)
+  }
+
+function getinhere() {
+let yikes = reverse(capitalize());
+return word.concat(yikes);
+} 
+
+function count() {
+count(all letters divide by 2, out letter at index)
+}
+
+console.log(capitalize(word));
+console.log(reverse(capitalize));
+console.log(getinhere(word));
